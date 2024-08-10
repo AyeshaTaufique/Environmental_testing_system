01dfff7b32eac1c2dd6aa49d4bdfd1934cba3f4a
AyeshaTaufique <148105714+AyeshaTaufique@users.noreply.github.com>
2024-08-10 23:51:22 +0500

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+cd ~/Desktop
+gcc try.c report_email_sender.c cJSON.c -o test -lcurl
+
+if [ $? -eq 0 ]; then
+ 
+
+  while true; do
+    # Run the compiled program
+    ./test
+    exit_code=$?
+
+    # Check the exit code of the program
+    if [ $exit_code -eq 0 ]; then
+      echo "Program executed successfully."
+      echo " "
+    else
+      echo "Program exited with an error. Exit code: $exit_code"
+    fi
+
+    # Wait for the next iteration
+    echo "Waiting for the next iteration..."
+    echo " "
+    sleep 10
+  done
+else
+  echo "Compilation failed. Please check the errors."
+fi
